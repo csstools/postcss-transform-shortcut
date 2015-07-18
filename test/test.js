5e5abd45728527d875ffe98e3cfba848275b1d89
Jonathan Neal <jonathantneal@hotmail.com>
2015-07-17 22:45:12 -0400

    Update plugin scaffolding

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var postcss = require('postcss');
+var expect  = require('chai').expect;
+
+var plugin = require('../');
+
+var test = function (input, output, opts, done) {
+	postcss([ plugin(opts) ]).process(input).then(function (result) {
+		expect(result.css).to.eql(output);
+
+		expect(result.warnings()).to.be.empty;
+
+		done();
+	}).catch(function (error) {
+		done(error);
+	});
+};
